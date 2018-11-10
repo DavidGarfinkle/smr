cfef14ff56bf1c9c3ebe746944baa8c557feba51
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2018-11-10 12:20:30 -0500

    Allow flask-api to be installed locally as editable

---
+++ flask-api/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='smr-flask',
+        version='0.1dev',
+        description='Flask appliction serving a RESTful API to an indexed music database',
+        author='David Garfinkle',
+        author_email='david.garfinkle@mail.mcgill.ca',
+        url='https://github.com/davidgarfinkle/smr/',
+        packages=['server']
+        )
