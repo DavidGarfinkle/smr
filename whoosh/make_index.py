bc4cf77f0e55b8c6afcc1230e27aaa893100a46b
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2018-11-16 01:14:02 -0500

    Adding the palestrina masses to whoosh index

--- whoosh/make_index.py
+++ whoosh/make_index.py
@@ -1,12 +1,32 @@
 import os
+import music21
 from whoosh.fields import Schema, STORED, ID, KEYWORD, TEXT
 from whoosh.index import create_in
 
-INDEX_PATH = "/usr/share/smr-db/whoosh-index"
+us = music21.environment.UserSettings()
+us.restoreDefaults()
 
-schema = Schema(title=TEXT(stored=True), content=TEXT, path=ID(stored=True), tags=KEYWORD, icon=STORED)
+DATA_PATH = os.path.abspath(os.path.join(os.sep, 'usr', 'share', 'smr-db'))
+INDEX_PATH = os.path.join(DATA_PATH, 'whoosh-index')
+
+schema = Schema(title=TEXT(stored=True), content=STORED, path=ID(stored=True), tags=KEYWORD)
 
 if not os.path.exists(INDEX_PATH):
     os.mkdir(INDEX_PATH)
 ix = create_in(INDEX_PATH, schema)
+
+writer = ix.writer()
+
+# PALESTRINA MASSES
+palestrina_path = os.path.join(DATA_PATH, 'palestrina-masses-xml')
+palestrina_masses = os.listdir(palestrina_path)
+for mass in palestrina_masses:
+    with open(os.path.join(palestrina_path, mass), 'r') as f:
+        xml = f.read()
+    writer.add_document(
+        title=mass,
+        content=xml,
+        path=os.path.join(palestrina_path, mass),
+        tags="mass,palestrina"
+    )
 
